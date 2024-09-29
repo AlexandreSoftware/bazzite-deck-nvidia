49ef1a716101bd2e36947f79473621b10426e7b0
Alexandre Oliveira <44621106+AlexandreSoftware@users.noreply.github.com>
2024-09-29 15:19:43 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,11 +14,12 @@ RELEASE="$(rpm -E %fedora)"
 
 # this installs a package from fedora repos
 rpm-ostree install screen
+
+curl -L https://copr.fedorainfracloud.org/coprs/gloriouseggroll/nobara-40/repo/fedora-40/gloriouseggroll-nobara-40-fedora-40.repo > /etc/yum.repos.d/nobara.repo
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
-#rpm-ostree install gamescope gamescope-session-plus gamescope-session-steam gamescope-htpc-common
-sudo rpm-ostree install akmod-nvidia xorg-x11-drv-nvidia
-sed "205,1s/^/export LD_PRELOAD=\"~\/CustomSteamPrerunScript.so\"/" /usr/bin/steam  > /usr/bin/steam
+rpm-ostree install gamescope gamescope-session-plus gamescope-session-steam gamescope-htpc-common
+sed "205,1s/^/export LD_PRELOAD='~\/CustomSteamPrerunScript.so'/" /usr/bin/steam > /usr/bin/steam
 
 #### Example for enabling a System Unit File
 
